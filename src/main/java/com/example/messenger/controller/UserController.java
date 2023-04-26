c5ba6acfac59dc31aec8a491989faf72a0b5aadf
denis <den.moskvin2045@mail.ru>
2023-04-26 18:35:22 +0700

    Сделал базовую аунтификацию и регистрацию

---
+++ src/main/java/com/example/messenger/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.example.messenger.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class UserController {
+    @GetMapping("/chat")
+    public String welcome(){
+        return "chat";
+    }
+}
---
+++ src/test/java/com/example/messenger/MessengerSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.messenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessengerSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
