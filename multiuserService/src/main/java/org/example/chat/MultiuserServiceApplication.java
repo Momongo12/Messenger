a8066c991c946589075bcbacb11934941b6a7620
denis <den.moskvin2045@mail.ru>
2023-07-28 23:54:23 +0500

    Создал сервис многопользовательских чат комнат

---
+++ multiuserService/src/main/java/org/example/chat/MultiuserServiceApplication.java
@@ -0,0 +1,12 @@
+package org.example.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiuserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiuserServiceApplication.class, args);
+	}
+}
